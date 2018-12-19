8f2210ddb11e3109bf0d7ff27b17dbc8a6fb2b09
markcor <mcornmesser@mozilla.com>
2018-12-19 11:08:09 -0800

    add comments

--- manifests/disable_service.pp
+++ manifests/disable_service.pp
@@ -1,5 +1,8 @@
 class services::disable_service {
 
+# Bug list:
+# https://bugzilla.mozilla.org/show_bug.cgi?id=1510759
+
 	$disbaled_services = ["wsearch", "VSS", "puppet"]
 	
 	service { $disbaled_services :
