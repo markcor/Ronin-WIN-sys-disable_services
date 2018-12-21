94b1b704444c33945343505da457c8ab4f4a1a5b
markcor <mcornmesser@mozilla.com>
2018-12-21 08:39:50 -0800

    <>

--- manifests/disable_service.pp
+++ manifests/disable_service.pp
@@ -1,7 +1,8 @@
-class services::disable_service {
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Bug list:
-# https://bugzilla.mozilla.org/show_bug.cgi?id=1510759
+class services::disable_service {
 
 	$disbaled_services = ["wsearch", "VSS", "puppet"]
 	
