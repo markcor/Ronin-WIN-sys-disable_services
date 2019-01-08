7fd63e5c8c8e41f2645228f50672573ecf5f76fd
markcor <mcornmesser@mozilla.com>
2019-01-08 08:49:53 -0800

    define disable_service

--- manifests/disable_service.pp
+++ manifests/disable_service.pp
@@ -2,11 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class win_services::disable_service {
+define win_services::disable_service {
 
-	$disbaled_services = ["wsearch", "VSS", "puppet"]
-	
-	service { $disbaled_services :
+	service { $service:
 		ensure => 'stopped',
 		enable => 'false', 
 	}
