8810ad92061b6e25e8e7fce54987d7eae83173f0
markcor <mcornmesser@mozilla.com>
2019-01-08 09:30:54 -0800

    :::

--- manifests/disable_service.pp
+++ manifests/disable_service.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define win_services::disable_service {
+define win_services::disable_service ($service=$title) {
 
 	service { $service:
 		ensure => 'stopped',
